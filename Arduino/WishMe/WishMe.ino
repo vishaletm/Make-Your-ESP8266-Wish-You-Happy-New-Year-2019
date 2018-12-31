884e73e18a8be93f7d3784eddbe75cd71dcb27d0
Vishal <vishaletm@gmail.com>
2018-12-31 21:21:21 +0530

    First Commit

---
+++ Arduino/WishMe/WishMe.ino
@@ -0,0 +1,94 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>   // Include the SPIFFS library
+const char* ssid = "VKM";
+const char* password = "edcwsxqaz";
+
+ESP8266WebServer server(80);
+
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+  const char MAIN_page[] PROGMEM = R"=====(
+<HTML>
+</HTML>
+)=====";
+
+
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  SPIFFS.begin(); 
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", []() {
+    
+   if (SPIFFS.exists("/index.html")) {                            // If the file exists
+    File file = SPIFFS.open("/index.html", "r");                 // Open it
+   server.send(200, "text/plain",""); // And send it to the client
+    file.close();                                       // Then close the file again
+    return true;
+  }
+  else
+{
+   Serial.println("File Not opened");
+  }
+
+   // server.send(200, "text/html", MAIN_page);
+   
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+}
